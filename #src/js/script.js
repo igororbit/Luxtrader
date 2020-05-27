ecb0853474fd2d8747ffd436138e5078c93a6a3c
igororbit <37049299+igororbit@users.noreply.github.com>
2020-05-27 22:54:48 +0300

    27.05.20 22.54

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,4 +1,7 @@
 @@include('files/webpcss.js')
 @@include('files/alert.js')
 @@include('files/user-header__menu.js')
-@@include('files/script.js')
+
+
+@@include('files/js/app.js')
+@@include('files/js/vendors.js')
