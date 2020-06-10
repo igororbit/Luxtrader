ab6f95925fe1a524273e9722942c63727a32c02a
igororbit <37049299+igororbit@users.noreply.github.com>
2020-06-11 00:10:23 +0300

    11.06 0.10

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,8 +1,6 @@
 @@include('files/webpcss.js')
-@@include('files/alert.js')
 @@include('files/user-header__menu.js')
 @@include('files/click_close.js')
 
 
 @@include('files/js/app.js')
-@@include('files/js/vendors.js')
