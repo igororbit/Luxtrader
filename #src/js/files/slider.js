71a91d3fc875501b8cb76eeae2a2513af9fac99b
igororbit <37049299+igororbit@users.noreply.github.com>
2020-06-12 19:54:32 +0300

    12.06 19.54

---
+++ #src/js/files/slider.js
@@ -0,0 +1,82 @@
+var sliders = document.querySelectorAll('._swiper');
+
+if (sliders) {
+   for (var _index24 = 0; _index24 < sliders.length; _index24++) {
+      var slider = sliders[_index24];
+
+      if (!slider.classList.contains('swiper-bild')) {
+         var slider_items = slider.children;
+
+         if (slider_items) {
+            for (var _index25 = 0; _index25 < slider_items.length; _index25++) {
+               var _el12 = slider_items[_index25];
+
+               _el12.classList.add('swiper-slide');
+            }
+         }
+
+         var slider_content = slider.innerHTML;
+         var slider_wrapper = document.createElement('div');
+         slider_wrapper.classList.add('swiper-wrapper');
+         slider_wrapper.innerHTML = slider_content;
+         slider.innerHTML = '';
+         slider.appendChild(slider_wrapper);
+         slider.classList.add('swiper-bild');
+      }
+
+      if (slider.classList.contains('_gallery')) {//slider.data('lightGallery').destroy(true);
+      }
+   }
+
+   sliders_bild_callback();
+}
+
+function sliders_bild_callback(params) { }
+
+var main_slider = new Swiper('.main-slider__body', {
+	/*
+	effect: 'fade',
+	autoplay: {
+		  delay: 3000,
+		  disableOnInteraction: false,
+	},
+	*/
+   observer: true,
+   observeParents: true,
+   slidesPerView: 1,
+   spaceBetween: 0,
+   ///autoHeight: true,
+   speed: 800,
+   //touchRatio: 0,
+   //simulateTouch: false,
+   loop: true,
+   //preloadImages: false,
+   //lazy: true,
+   // Dotts
+   //pagination: {
+   //	el: '.slider-quality__pagging',
+   //	clickable: true,
+   //},
+   // Arrows
+   navigation: {
+      nextEl: '.control-main-slider__arrow_next',
+      prevEl: '.control-main-slider__arrow_prev'
+   },
+   breakpoints: {
+      320: {
+         autoHeight: true
+      },
+      768: {
+         autoHeight: false
+      }
+   },
+   on: {
+      lazyImageReady: function lazyImageReady() {
+         ibg();
+      }
+   } // And if we need scrollbar
+   //scrollbar: {
+   //	el: '.swiper-scrollbar',
+   //},
+
+});
--- #src/js/script.js
+++ #src/js/script.js
@@ -1,8 +1,7 @@
-
-
 @@include('files/#header/user-header__menu.js')
 @@include('files/#header/click_close.js')
 
-
 @@include('files/webpcss.js')
-@@include('files/app.js')
+
+@@include('libs/swiper.js')
+@@include('files/slider.js')
